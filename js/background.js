4634bef24c77aa231c29d14692429e00039fd846
almirus <almir.abrarov@gmail.com>
2017-09-13 23:31:46 +0300

    [-] удалена библиотека jquery [+] используются промисы

--- js/background.js
+++ js/background.js
@@ -1,30 +1,67 @@
 function getTCSsession() {
-    console.log('try to get cookies');
-    var session_id;
-    chrome.cookies.getAll({}, function (cookie) {
-        var psid = cookie.filter(function (value) {
-            return value.name == 'psid';
+    return new Promise(function (resolve, reject) {
+        console.log('try to get cookies');
+        chrome.cookies.getAll({}, function (cookie) {
+            var psid = cookie.filter(function (value) {
+                return value.name == 'psid';
+            });
+            if (psid[0].value) {
+                console.log('psid founded');
+                // пробуем проверить полученную печеньку
+                fetch('https://api01.tinkoff.ru/v1/personal_info?sessionid=' + psid[0].value)
+                    .then(function (response) {
+                        return response.json()
+                    }).then(function (json) {
+                    console.log('parsed json', json);
+                    if (json.resultCode=='OK') {
+                        resolve(psid[0].value);
+                    }else{
+                        reject(undefined);
+                    }
+
+                }).catch(function (ex) {
+                    console.log('parsing failed', ex)
+                });
+
+            } else {
+                console.log('psid not found');
+                reject(undefined);
+            }
         });
-        session_id=psid[0].value;
-    });
-    return session_id;
+    })
 }
 
 function getSum() {
-    var session_id = getTCSsession();
-    console.log('ssid='+session_id);
-    if (session_id) {
-        $.getJSON('https://api.tinkoff.ru/trading/portfolio/purchased_securities?sessionId=' + session_id, function (data) {
-            console.log('sum='+data.payload.totalAmountStocks);
-            return data.payload.totalAmountStocks;
-        });
-    }else{
-        return 'Ошибка получения сумм'
-    }
-};
-chrome.runtime.onMessage.addListener(function(message,sender,sendRepsonse){
-    if(message.method == "getSum")
-        chrome.runtime.sendMessage(getSum(), function (response) {
-            ;
+    getTCSsession().then(function (session_id) {
+        fetch('https://api.tinkoff.ru/trading/portfolio/purchased_securities?sessionId=' + session_id)
+            .then(function (response) {
+                return response.json()
+            }).then(function (json) {
+            console.log('parsed json', json)
+
+        }).catch(function (ex) {
+            console.log('parsing failed', ex)
         })
+    }).catch(function () {
+        // редиректим на страницу логина
+    })
+}
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendRepsonse) {
+    if (message.method == "getSum")
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {sum: getSum()}, function(response) {
+                console.log(response);
+            });
+        });
 });
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        console.log(request);
+        if (request.method == "getSum")
+            sendResponse({sum: getSum()});
+    });
+
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,8 @@
-$(function() {
+
     chrome.runtime.sendMessage({method:"getSum"},function(reply){
-        $('#sum').text(reply);
+        chrome.extension.getBackgroundPage().console.log('get sum from background ='+reply);
     });
-});
+    chrome.runtime.onMessage.addListener (function (request, sender, sendResponse) {
+        chrome.extension.getBackgroundPage().console.log('get sum from background '+request.sum);
+        document.getElementById('sum').innerText=request.sum;
+    });
