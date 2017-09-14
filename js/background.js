6e602256c36bee5379e67c1f6329ad8b60aab55a
almirus <almir.abrarov@gmail.com>
2017-09-14 18:26:59 +0300

    [*] переделано отправка сообщений между страницами

--- js/background.js
+++ js/background.js
@@ -1,28 +1,48 @@
+const INFO_URL = 'https://api01.tinkoff.ru/v1/personal_info?sessionid=';
+const TRADING_PURCHASED_URL = 'https://api.tinkoff.ru/trading/portfolio/purchased_securities?sessionId=';
+const LOGIN_URL='https://www.tinkoff.ru/login/?redirectTo=%2Finvest%2F';
+const HOST_URL='https://www.tinkoff.ru/';
+/**
+ * проверяем что сессия жива
+ * @param {string} значение sessionId
+ * @return {boolean} можно использовать или нет
+ */
+function sessionIsAlive(sessionId) {
+    fetch(INFO_URL + sessionId)
+        .then(function (response) {
+            return response.json()
+        }).then(function (json) {
+            if (json.resultCode=='OK') {
+                console.log('session is alive');
+                return true;
+            }else{
+                console.log('session is dead');
+                return false;
+            }
+        }).catch(function (ex) {
+            console.log('parsing failed', ex);
+            return false;
+        });
+}
+/**
+ * получаем сессию пользователя из cookies + проверка что она валидна
+ * @return {object} - возвращается строка с sessionId через promise
+ */
 function getTCSsession() {
     return new Promise(function (resolve, reject) {
         console.log('try to get cookies');
         chrome.cookies.getAll({}, function (cookie) {
-            var psid = cookie.filter(function (value) {
-                return value.name == 'psid';
+            let psid = cookie.filter(function (value) {
+                // нам нужен только один единственный с именем psid
+                return value.name === 'psid';
             });
             if (psid[0].value) {
                 console.log('psid founded');
-                // пробуем проверить полученную печеньку
-                fetch('https://api01.tinkoff.ru/v1/personal_info?sessionid=' + psid[0].value)
-                    .then(function (response) {
-                        return response.json()
-                    }).then(function (json) {
-                    console.log('parsed json', json);
-                    if (json.resultCode=='OK') {
-                        resolve(psid[0].value);
-                    }else{
-                        reject(undefined);
-                    }
-
-                }).catch(function (ex) {
-                    console.log('parsing failed', ex)
-                });
-
+                if (sessionIsAlive(psid[0].value)){
+                    resolve(psid[0].value)
+                } else {
+                    reject(undefined);
+                }
             } else {
                 console.log('psid not found');
                 reject(undefined);
@@ -31,26 +51,51 @@ function getTCSsession() {
     })
 }
 
-function getSum() {
+/**
+ * получаем полную стоимость всех бумаг
+ * @return {float} - сумма
+ */
+function getAllSum() {
+    console.log('try to get sums');
     getTCSsession().then(function (session_id) {
-        fetch('https://api.tinkoff.ru/trading/portfolio/purchased_securities?sessionId=' + session_id)
+        fetch(TRADING_PURCHASED_URL + session_id)
             .then(function (response) {
                 return response.json()
             }).then(function (json) {
-            console.log('parsed json', json)
-
+                console.log('parsed json', json);
+            return json.payload.totalAmountStocks;
         }).catch(function (ex) {
             console.log('parsing failed', ex)
         })
     }).catch(function () {
         // редиректим на страницу логина
+        chrome.tabs.query({url:HOST_URL+'*'}, function(tabs) {
+            if(tabs.length) {
+                // если был открыт - обновляем
+                chrome.tabs.reload(tabs[0].id);
+                chrome.tabs.update(tabs[0].id, {selected: true, url: LOGIN_URL});
+            } else {
+                // иначе - открываем заново
+                chrome.tabs.create({active: true, url: LOGIN_URL});
+            }
+        });
+
     })
 }
 
+/**
+ * обновляем все данные на форме
+ * @return {object} данные для отображения на форме
+ */
+function updatePopup(){
+
+}
+
+/*
 chrome.runtime.onMessage.addListener(function (message, sender, sendRepsonse) {
-    if (message.method == "getSum")
+    if (message.method == "getAllSum")
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {sum: getSum()}, function(response) {
+            chrome.tabs.sendMessage(tabs[0].id, {sum: getAllSum()}, function(response) {
                 console.log(response);
             });
         });
@@ -61,7 +106,17 @@ chrome.runtime.onMessage.addListener(
             "from a content script:" + sender.tab.url :
             "from the extension");
         console.log(request);
-        if (request.method == "getSum")
-            sendResponse({sum: getSum()});
+        if (request.method == "getAllSum")
+            sendResponse({sum: getAllSum()});
     });
+*/
 
+chrome.extension.onConnect.addListener(function(port) {
+    console.log("Connected .....");
+    port.onMessage.addListener(function(msg) {
+        console.log("message recieved " + JSON.stringify(msg));
+        if (msg.method == "getAllSum") {
+            port.postMessage({sum: getAllSum()});
+        }
+    });
+});
--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,19 @@
+/*
+chrome.runtime.sendMessage({method: "getAllSum"}, function (reply) {
+    chrome.extension.getBackgroundPage().console.log('get sum from background =' + JSON.stringify(reply));
+});
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    chrome.extension.getBackgroundPage().console.log('get sum from background ' + request.sum);
+    document.getElementById('sum').innerText = request.sum;
+});
+*/
 
-    chrome.runtime.sendMessage({method:"getSum"},function(reply){
-        chrome.extension.getBackgroundPage().console.log('get sum from background ='+reply);
-    });
-    chrome.runtime.onMessage.addListener (function (request, sender, sendResponse) {
-        chrome.extension.getBackgroundPage().console.log('get sum from background '+request.sum);
-        document.getElementById('sum').innerText=request.sum;
-    });
+let port = chrome.extension.connect({
+    name: "tcs_trader"
+});
+
+port.postMessage({method: "getAllSum"});
+
+port.onMessage.addListener(function(msg) {
+    chrome.extension.getBackgroundPage().console.log('get sum from background ' + msg.sum);
+});
