791c130194c4edcfe388478e9f2118e765422a34
almirus <almir.abrarov@gmail.com>
2018-11-27 21:28:06 +0300

    [+] добавлен функционал отслеживания цен

---
+++ js/constants.mjs
@@ -0,0 +1,12 @@
+export const INFO_URL = 'https://api.tinkoff.ru/v1/session_status?sessionid=';
+export const TRADING_PURCHASED_URL = 'https://api.tinkoff.ru/trading/portfolio/purchased_securities?sessionId=';
+export const LOGIN_URL = 'https://www.tinkoff.ru/login/?redirectTo=/invest/broker_account/';
+export const HOST_URL = 'https://www.tinkoff.ru/';
+export const ALL_ACCOUNTS = 'https://api.tinkoff.ru/trading/portfolio/all_accounts?sessionId=';
+export const BUY_LINK = 'https://www.tinkoff.ru/invest/buy/';
+export const SELL_LINK = 'https://www.tinkoff.ru/invest/sell/';
+export const PING_URL = 'https://api.tinkoff.ru/v1/ping?sessionid=';
+export const INTERVAL_TO_CHECK=1;
+
+//export const EXTENSION_ID='gggmpnfbhgpbglfenigemnnlfhjddcid';
+export const EXTENSION_ID='heplhpapcgfjfajhnabkjcmgcfgfbbhh';
