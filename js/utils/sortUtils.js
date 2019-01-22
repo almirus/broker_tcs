389cf8ccf5fb30935bf98985eb1ef3881af991f3
dravyman <drav10@rambler.ru>
2019-01-22 20:17:53 +0300

    Поправил сортировку.
    Добавил прозрачность для тех бумаг, которым далеко до цели + вывожу колонку с указанием процентов до цели

--- js/utils/sortUtils.js
+++ js/utils/sortUtils.js
@@ -1,31 +1,18 @@
 'use strict';
 
 export function sortAlertRow(first, second) {
-    return giveLessDiffToTarget(first) > giveLessDiffToTarget(second)
+    let firstNum = giveLessDiffToTarget(first);
+    let secondNum = giveLessDiffToTarget(second);
+    if (firstNum < secondNum) return -1;
+    if (firstNum > secondNum) return 1;
+    return 0;
 }
 
 export function giveLessDiffToTarget(desireTicker) {
     let diffToBuy = 100;
     let diffToSell = 100;
-    if (desireTicker.buy_price) diffToBuy = (desireTicker.online_buy_price - desireTicker.buy_price) / desireTicker.online_buy_price;
-    if (desireTicker.sell_price) diffToSell = (desireTicker.sell_price - desireTicker.online_sell_price) / desireTicker.online_sell_price;
+    if (desireTicker.buy_price && desireTicker.online_buy_price) diffToBuy = (desireTicker.online_buy_price - desireTicker.buy_price) / desireTicker.online_buy_price;
+    if (desireTicker.sell_price && desireTicker.online_sell_price) diffToSell = (desireTicker.sell_price - desireTicker.online_sell_price) / desireTicker.online_sell_price;
+    if (diffToBuy + diffToSell === 200) return 0;
     return (diffToBuy < diffToSell) ? diffToBuy : diffToSell;
 }
-
-// {
-//     active: true
-//     best_before: ""
-//     buy_price: "66.22"
-//     currency: "RUB"
-//     earnings:
-//         absolute: {currency: "RUB", value: 0.0525}
-//     relative: 0.0007907817442385902
-//     __proto__: Object
-//     exchangeStatus: "Open"
-//     online_average_price: 66.4425
-//     online_buy_price: 66.445
-//     online_sell_price: 66.43
-//     sell_price: ""
-//     showName: "Доллар США"
-//     ticker: "USDRUB"
-// }
