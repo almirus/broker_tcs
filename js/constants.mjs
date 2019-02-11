3d3a3e6b360bd3f2f2971e18b42ef68ce5bf6c55
almirus <almir.abrarov@gmail.com>
2019-02-11 13:37:07 +0300

    [*] изменение в константах

--- js/constants.mjs
+++ js/constants.mjs
@@ -2,8 +2,8 @@
 export const INFO_URL = 'https://api.tinkoff.ru/v1/session_status?sessionid=';
 export const LOGIN_URL = 'https://www.tinkoff.ru/login/?redirectTo=/invest/broker_account/';
 export const HOST_URL = 'https://www.tinkoff.ru/';
-export const FAVORITE_URL = 'https://api.tinkoff.ru/trading//user/get_favorites?sessionId=';
-export const ADD_FAFORITE_URL = 'https://api.tinkoff.ru/trading//user/add_to_favorites?sessionId=';
+export const FAVORITE_URL = 'https://api.tinkoff.ru/trading/user/get_favorites?sessionId=';
+export const ADD_FAFORITE_URL = 'https://api.tinkoff.ru/trading/user/add_to_favorites?sessionId=';
 export const PORTFOLIO_URL = 'https://api.tinkoff.ru/trading/portfolio/purchased_securities?sessionId=';
 export const ALL_ACCOUNTS = 'https://api.tinkoff.ru/trading/portfolio/all_accounts?sessionId=';
 export const BUY_LINK = 'https://www.tinkoff.ru/invest/buy/';
@@ -20,9 +20,8 @@ export const CURRENCY_SYMBOL_URL = 'https://api.tinkoff.ru/trading/currency/get?
 export const PROGNOSIS_URL = 'https://api.tinkoff.ru/trading/stocks/prognosis?ticker=${ticker}&sessionId=';
 export const SET_ALERT_URL = 'https://api.tinkoff.ru/trading/subscriptions/price_alert?ticker=${ticker}&price=${price}&sessionId=';
 export const ORDERS_URL = 'https://api.tinkoff.ru/trading/user/orders?sessionId=';
-export const CANCEL_ORDER ='https://api.tinkoff.ru/trading/order/cancel?orderId=${orderId}&sessionId=';
-export const CREATE_ORDER ='https://api.tinkoff.ru/trading/order/limit_order?ticker=${ticker}&price=${price}&quantity=${quantity}&side=${side}&sessionId=';
-
+export const CANCEL_ORDER = 'https://api.tinkoff.ru/trading/order/cancel?orderId=${orderId}&sessionId=';
+export const CREATE_ORDER = 'https://api.tinkoff.ru/trading/order/limit_order?ticker=${ticker}&price=${price}&quantity=${quantity}&side=${side}&sessionId=';
 
 
 export const SEARCH_URL = 'https://api.tinkoff.ru/trading/stocks/list?sessionId=';
@@ -47,8 +46,8 @@ export const OPTION_SORT_BY_NEAREST = 'sort_by_nearest';
 export const TICKER_LIST = 'tickerList';
 export const ALERT_TICKER_LIST = 'alertTickerList';
 //up to 5 API requests per minute and 500 requests per day
-export const  ALPHAVANTAGE_KEY = 'M3JMJM8U22EIIO2Y';
-export  const AV_SYMBOL_URL = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=';
+export const ALPHAVANTAGE_KEY = 'M3JMJM8U22EIIO2Y';
+export const AV_SYMBOL_URL = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=';
 
 export const USD_RUB = 'USDRUB';
 
