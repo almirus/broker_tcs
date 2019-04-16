91f3bec359a12317a91bde9a3a45d6be01ac9e06
almirus <almir.abrarov@gmail.com>
2019-04-16 23:12:41 +0300

    [+] подготовка для вывода графиков

--- js/utils/displayUtils.js
+++ js/utils/displayUtils.js
@@ -33,7 +33,7 @@ function zerofill(number, length) {
     let result = number.toString();
     let pad = length - result.length;
 
-    while(pad > 0) {
+    while (pad > 0) {
         result = '0' + result;
         pad--;
     }
@@ -48,7 +48,7 @@ export function msToTime(s) {
     s = (s - secs) / 60;
     let mins = s % 60;
     let hrs = (s - mins) / 60;
-    return zerofill(hrs,2) + 'ч ' + zerofill(mins,2) + 'мин';
+    return zerofill(hrs, 2) + 'ч ' + zerofill(mins, 2) + 'мин';
 }
 
 export function getAllAccountsHtmlInfo(accounts) {
@@ -60,9 +60,9 @@ export function getAllAccountsHtmlInfo(accounts) {
 }
 
 function getAccountHtmlInfo(accountName, accountInfo) {
-    let rusAccountName = (accountName === 'Bcs') ?        "БКС" :
-                         (accountName === 'Tinkoff') ?    "ТКС" :
-                         (accountName === 'TinkoffIis') ? "ИИС" : accountName;
+    let rusAccountName = (accountName === 'Bcs') ? "БКС" :
+        (accountName === 'Tinkoff') ? "ТКС" :
+            (accountName === 'TinkoffIis') ? "ИИС" : accountName;
 
     let htmlTotalAmount = `<span style="font-weight: bold">${toCurrency(accountInfo.totalAmountPortfolio)}</span>`;
     let htmlExpectedYieldPerDay = `<span style="font-weight: bold" class="${accountInfo.expectedYieldPerDay > 0 ? 'onlineBuy' : 'onlineSell'}">${toCurrency(accountInfo.expectedYieldPerDay)}</span>`;
@@ -92,4 +92,107 @@ export function drawDayProgress(element) {
     ctx.fillStyle = progress_style;
     ctx.fillRect(dayOpenPercent, 0, dayLastPercent, 6);
     return canvas;
+}
+
+
+export function drawGraph(data, canvas) {
+    function getPoints(data) {
+        let points = [];
+        let len = data.length;
+        let sum = 0;
+        let count_valid = 0;
+        let max;
+        let min;
+        let d;
+        for (let i = 0; i < len; i++) {
+            d = data[i];
+            if (typeof d === 'number') {
+                if (typeof max !== 'number') {
+                    max = d;
+                    min = d;
+                }
+                max = d > max ? d : max;
+                min = d < max ? d : min;
+                count_valid += 1;
+                sum += data[i];
+            }
+        }
+        let average = sum / count_valid;
+        let middle = (max - min) / 2;
+        let range = max - min;
+        for (let i = 0; i < len; i++) {
+            d = data[i];
+            if (typeof d === 'number') {
+                points.push({
+                    val: 2 * ((d - min) / range - 0.5),
+                    data: d,
+                    index: i
+                });
+            } else {
+                points.push(null);
+            }
+        }
+        return points;
+    }
+
+    let el = document.createElement('canvas');
+
+    let len = data.length;
+    let width = parseInt(el.attr('width'), 10);
+    let height = parseInt(el.attr('height'), 10);
+    let gap = width / (len - 1);
+    let ctx = el.getContext('2d');
+    let startPoint = null;
+    let points = getPoints(data);
+    let endPoint;
+    let point;
+    for (let i = 0; i < len; i++) {
+        point = points[i];
+        if (point) {
+            if (!startPoint) {
+                startPoint = point;
+            }
+            endPoint = point;
+        }
+    }
+    if (!endPoint) {
+        return;
+    }
+    ctx.save();
+    ctx.fillStyle = '#f2f2f2';
+    ctx.lineWidth = '3';
+    ctx.fillRect(0, 0, width, height);
+    ctx.restore();
+    ctx.beginPath();
+    for (let i = 1; i < len; i++) {
+        ctx.moveTo(i * gap, 0);
+        ctx.lineTo(i * gap, height);
+    }
+    ctx.save();
+    ctx.strokeStyle = '#DDD';
+    ctx.stroke();
+    ctx.restore();
+    ctx.beginPath();
+    ctx.moveTo(startPoint.index * gap, height);
+
+    for (let i = 0; i < len; i++) {
+        point = points[i];
+        if (point) {
+            ctx.lineTo(point.index * gap, -point.val * height * 0.8 / 2 + height / 2);
+        }
+    }
+    ctx.lineTo(endPoint.index * gap, height);
+    ctx.save();
+    ctx.fillStyle = 'rgba(8,106,253,.4)';
+    ctx.strokeStyle = '#086afc';
+    ctx.lineWidth = '2';
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore();
+    ctx.save();
+    ctx.strokeStyle = '#666';
+    ctx.lineWidth = '3';
+    ctx.strokeRect(0, 0, width, height);
+    ctx.restore();
+    document.getElementById(canvas).appendChild(el);
 }
